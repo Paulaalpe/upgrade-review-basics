903139a3eb6126442687b3ea83515b560e682eb2
Paulaalpe <91777530+Paulaalpe@users.noreply.github.com>
2021-11-03 20:40:52 +0100

    Iteration#2

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 /***Iteración #1: Mix for e includes**      Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función **.includes()**
 */
-const movies = [
-    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
-    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
-    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
-    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+// const movies = [
+//     {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+//     {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+//     {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+//     {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+// ]
+
+// let categorias = [];
+// for(let movie of movies){
+//     let categoriasTemporal = movie.categories;
+//     for(let i=0;i<categoriasTemporal.length;i++) {
+//         if (!categorias.includes(categoriasTemporal[i])) {
+//             categorias.push(categoriasTemporal[i]);
+//         }
+//     }
+// }
+// console.log(categorias);
+
+/***Iteración #2: Mix Fors**    Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios */
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
 ]
 
-let categorias = [];
-for(let movie of movies){
-    let categoriasTemporal = movie.categories;
-    for(let i=0;i<categoriasTemporal.length;i++) {
-        if (!categorias.includes(categoriasTemporal[i])) {
-            categorias.push(categoriasTemporal[i]);
-        }
+let volumenes = [];
+for(let user of users){
+    let sonidosFavoritos = user.favoritesSounds;
+    for(let clave in sonidosFavoritos){
+        let formatoYVolumen = sonidosFavoritos[clave];
+        volumenes.push(formatoYVolumen.volume);      
     }
+};
+
+let totalVolumenes = volumenes.length;
+let sumaVolumenes = 0;
+for(let i=0;i<volumenes.length;i++){
+    sumaVolumenes += volumenes[i];  
 }
-console.log(categorias);
+let mediaVolumenes = sumaVolumenes / totalVolumenes;
+console.log(mediaVolumenes);
 
